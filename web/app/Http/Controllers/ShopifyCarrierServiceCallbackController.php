2ba1ff084da3ebe8eeab64a849c3ac5bd5fd3669
tyrone-digitalcake <tyrone@digitalcake.agency>
2024-10-14 11:16:45 +0100

    Added backwards compatibility for carrier service callback

--- web/app/Http/Controllers/ShopifyCarrierServiceCallbackController.php
+++ web/app/Http/Controllers/ShopifyCarrierServiceCallbackController.php
@@ -27,6 +27,57 @@ class ShopifyCarrierServiceCallbackController extends Controller
 
         $line_items = isset($rate['items']) ? collect($rate['items']) : collect([]);
 
+        $shipping_data_line_item = $line_items->first(fn ($item) => $item['properties'] && !empty($item['properties']['_shipping_methods']));
+
+        if ($shipping_data_line_item) {
+
+            $shipping_methods = json_decode($shipping_data_line_item['properties']['_shipping_methods'], true);
+
+            $selected_rates = collect([]);
+
+            foreach($shipping_methods as $addr_id => $rate_id) {
+                $rate = ShippingRate::where('id', $rate_id)->where('shop', $shop)->first();
+                if (!$rate) continue;
+                $selected_rates->push($rate);
+            }
+
+            $service_name = "";
+            $description = "";
+            $total_price = 0;
+            $service_code = "";
+
+            if ($selected_rates->count() < 2) {
+                $service_name = $selected_rates[0]->name;
+                $description = $selected_rates[0]->description;
+                $total_price += $selected_rates[0]->base_rate * 100;
+                $service_code = "cake_app_{$selected_rates[0]->id}";
+            } else {
+                $service_code = "cake_app_multiship";
+                $service_name = "Multiple shipping rates";
+                foreach($selected_rates as $rate) {
+                    if (!empty($description)) {
+                        $description .= " | ";
+                    }
+                    $rate_display_price = $rate->base_rate > 0 ? "£" . $rate->base_rate : "Free";
+                    $description .= $rate->name . " (" . $rate_display_price . ")";
+                    $total_price += $rate->base_rate * 100;
+                }
+            }
+
+            return [
+                'rates' => [
+                    [
+                        'service_name' => $service_name,
+                        'service_code' => $service_code,
+                        'total_price' => $total_price,
+                        'currency' => 'GBP',
+                        'description' => $description
+                    ]
+                ]
+            ];
+
+        }
+
         $shipment_ids = [];
 
         foreach($line_items as $line_item) {
